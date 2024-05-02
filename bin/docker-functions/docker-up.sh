f5b4a1aa23a29e4c7739d56263584fc97114cad9
TJ Draper <tdraper@rxante.com>
2024-05-02 10:56:25 -0500

    Add api.stmarkreformed.localtest.me

--- bin/docker-functions/docker-up.sh
+++ bin/docker-functions/docker-up.sh
@@ -37,7 +37,6 @@ function docker-up() {
     localMkCert "craft-slim-bridge.localtest.me";
     localMkCert "craft-scheduler.localtest.me";
     localMkCert "stmark.localtest.me";
-    localMkCert "stmarkreformed.localtest.me";
     localMkCert "2017.eeconf.localtest.me";
     localMkCert "2018.eeconf.localtest.me";
     localMkCert "2019.eeconf.localtest.me";
@@ -103,7 +102,9 @@ function docker-up() {
     localMkCert "qotd.mikedubose.localtest.me";
     localMkCert "lakemurrayboating.localtest.me";
     localMkCert "iisinet.localtest.me";
+    localMkCert "stmarkreformed.localtest.me";
     localMkCert "auth.stmarkreformed.localtest.me";
+    localMkCert "api.stmarkreformed.localtest.me";
 
     docker compose ${composeFiles} -p traefik-dev up -d;
 
